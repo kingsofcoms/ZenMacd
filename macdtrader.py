b84240edb74f08cef97b03a7ab5ef565f9753ca0
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-08-18 06:26:15 -0500

    Macd Buy Threshold Increase
    Macd Buy Threshold Increase

--- macdtrader.py
+++ macdtrader.py
@@ -168,7 +168,7 @@ def run():
         global buystr
         global sellstr
         # EXAMPLE WITH ALL STRINGS: word_list = ["BTC_DCR", "BTC_LTC", "BTC_NAUT", "BTC_NXT", "BTC_XCP", "BTC_GRC", "BTC_REP", "BTC_PPC", "BTC_RIC", "BTC_STRAT", "BTC_GAME", "BTC_BTM", "BTC_CLAM", "BTC_ARDR", "BTC_BLK", "BTC_OMNI", "BTC_SJCX", "BTC_FLDC", "BTC_BCH", "BTC_DOGE", "BTC_POT", "BTC_VRC", "BTC_ETH", "BTC_PINK", "BTC_NOTE", "BTC_BTS", "BTC_AMP", "BTC_NAV", "BTC_BELA", "BTC_BCN", "BTC_ETC", "BTC_FLO", "BTC_VIA", "BTC_XBC", "BTC_XPM", "BTC_DASH", "BTC_XVC", "BTC_GNO", "BTC_NMC", "BTC_RADS", "BTC_VTC", "BTC_XEM", "BTC_FCT", "BTC_XRP", "BTC_NXC", "BTC_STEEM", "BTC_SBD", "BTC_BURST", "BTC_XMR", "BTC_DGB", "BTC_LBC", "BTC_BCY", "BTC_PASC", "BTC_SC", "BTC_LSK", "BTC_EXP", "BTC_MAID", "BTC_BTCD", "BTC_SYS", "BTC_GNT", "BTC_HUC", "BTC_EMC2", "BTC_NEOS", "BTC_ZEC", "BTC_STR"]
-        word_list = ["BTC_DOGE", "BTC_BCN", "BTC_BCH", "BTC_LTC", "BTC_DASH", "BTC_ETH"]
+        word_list = ["BTC_BCH, "BTC_ETH", "BTC_LTC", "BTC_DASH", "BTC_XRP", "BTC_FCT", "BTC_DGB", "BTC_STRAT", "BTC_STR", "BTC_ETC"]
         # Let's just use 5 for now... keeps things going quicker.
         for word in word_list:
             time.sleep(5)
@@ -184,7 +184,8 @@ def run():
                 float1=m.group(1)
                 print(word, float1)
                 float2 = int(float(float1))
-                if float2 > .5:
+                # Set the below number for minimum macd buy 2 seems good to prevent worthless coins from skewing your results
+                if float2 > 2.5:
                     ke1=word.replace('BTC_', '')
                     ke3='-BTC'
                     ke8=ke1+ke3
