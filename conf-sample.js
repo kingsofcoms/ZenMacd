3c6590c10ebfda938846a21155a3725b7512ec4d
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-08-18 20:25:09 -0500

    Updates

--- conf-sample.js
+++ conf-sample.js
@@ -96,23 +96,23 @@ c.profit_stop_pct = 1
 
 // avoid trading at a slippage above this pct
 c.max_slippage_pct = 5
-// buy with this % of currency balance (WARNING : sim won't work properly if you set this value to 100)
-// 5 percent will support atleast 20 coins at about $20 using the macdtrader.py... Depends how much money you have.
-c.buy_pct = 5
+// buy with this % of currency balance (WARNING : sim won't work properly if you set this value to 100) 
+c.buy_pct = 100
 // sell with this % of asset balance (WARNING : sim won't work properly if you set this value to 100)
 c.sell_pct = 100
 // ms to adjust non-filled order after
-c.order_adjust_time = 10000
+c.order_adjust_time = 20000
 // avoid selling at a loss below this pct
 c.max_sell_loss_pct = 25
 // ms to poll order status
 c.order_poll_time = 10000
 // ms to wait for settlement (after an order cancel)
-c.wait_for_settlement = 10000
+c.wait_for_settlement = 5000
 // % to mark up or down price for orders
-c.markup_pct = 2
+c.markup_pct = 0
 // become a market taker (high fees) or a market maker (low fees)
 c.order_type = 'taker'
+
 
 // Misc options:
 
